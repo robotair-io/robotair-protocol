1add9d583359abc1ed5b075ba0f91a7bd29de552
Pedro Miguel Melo <pedro.m.melo@inesctec.pt>
2023-05-30 13:10:13 +0100

    Add request message for starting services

--- robotair_protocol/requests.py
+++ robotair_protocol/requests.py
@@ -21,3 +21,20 @@ class ImagePullRequest(BaseModel):
     ignore_pull_failures: bool = False
     include_deps: bool = True
     quiet: bool = True
+
+
+class ServiceStartRequest(BaseModel):
+
+    # NOTE: based on the arguments of
+    # https://gabrieldemarmiesse.github.io/python-on-whales/sub-commands/compose/#up
+
+    # required fields
+    services: Optional[List[str]]
+
+    # optional fields
+    abort_on_container_exit: bool = False
+    force_recreate: bool = True
+    detach: bool = True
+    quiet: bool = True
+    remove_orphans: bool = True
+    wait: bool = True
