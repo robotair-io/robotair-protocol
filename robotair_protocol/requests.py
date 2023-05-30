bc5048925889d322b076d24bae65dbbf978524aa
Pedro Miguel Melo <pedro.m.melo@inesctec.pt>
2023-05-30 13:09:41 +0100

    Add request message for pulling service images

--- robotair_protocol/requests.py
+++ robotair_protocol/requests.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import List, Optional
 from .compose import ComposeSpecification
 
 
@@ -6,3 +7,17 @@ class DeploymentUpdateRequest(BaseModel):
 
     # required fields
     deployment: ComposeSpecification
+
+
+class ImagePullRequest(BaseModel):
+
+    # NOTE: based on the arguments of
+    # https://gabrieldemarmiesse.github.io/python-on-whales/sub-commands/compose/#pull
+
+    # required fields
+    services: Optional[List[str]]
+
+    # optional fields
+    ignore_pull_failures: bool = False
+    include_deps: bool = True
+    quiet: bool = True
