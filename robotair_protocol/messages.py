b689ae4001f46993d8eb4810435a5f9818901895
Pedro Miguel Melo <pedro.m.melo@inesctec.pt>
2023-05-31 15:19:05 +0100

    Add messages required for basic deployment introspection.

--- robotair_protocol/messages.py
+++ robotair_protocol/messages.py
@@ -1,4 +1,30 @@
 from pydantic import BaseModel
+from typing import List
+
+
+class Image(BaseModel):
+
+    # required fields
+    tag: str
+    digest: str
+    size: int  # bytes
+
+
+class Service(BaseModel):
+
+    # required fields
+    state: str
+    image: Image
+
+    # optional fields
+    ram_usage: int = 0  # bytes
+    cpu_percentage: float = 0.0
+
+
+class DeploymentStatusMessage(BaseModel):
+
+    # required fields
+    services: List[Service]
 
 
 class ErrorMessage(BaseModel):
